6fd8c9294f3d44c51e039b881ef7d499eb275732
Roman Matanin <romashka951@gmail.com>
2022-09-08 14:31:36 +0300

    Создание полноценного шаблона для проектов с css и тестовой базой. Конец главы 18 стр 496

--- TemplateProject/Startup.cs
+++ TemplateProject/Startup.cs
@@ -7,19 +7,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using TemplateProject.Models;
 
 namespace TemplateProject
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opts => {
+                opts.UseSqlServer(Configuration["ConnectionStrings:ProductConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext context)
         {
             if (env.IsDevelopment())
             {
@@ -28,6 +41,7 @@ namespace TemplateProject
 
             app.UseRouting();
 
+            app.UseMiddleware<TestMiddleware>();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
@@ -35,6 +49,7 @@ namespace TemplateProject
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+            SeedData.SeedDatabase(context);
         }
     }
 }
---
+++ TemplateProject/TestMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using TemplateProject.Models;
+
+namespace TemplateProject
+{
+    public class TestMiddleware
+    {
+        private RequestDelegate _nextDelegate;
+        public TestMiddleware(RequestDelegate nextDelegate)
+        {
+            _nextDelegate = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context, DataContext dataContext)
+        {
+            if (context.Request.Path == "/test")
+            {
+                await context.Response.WriteAsync($"В базе {dataContext.Products.Count()} продуктов.",System.Text.Encoding.GetEncoding(1251));
+                await context.Response.WriteAsync($"\nВ базе {dataContext.Categories.Count()} категорий.", System.Text.Encoding.GetEncoding(1251));
+                await context.Response.WriteAsync($"\nВ базе {dataContext.Suppliers.Count()} поставщиков.", System.Text.Encoding.GetEncoding(1251));
+            }
+            else
+                await _nextDelegate(context);
+        }
+    }
+}
