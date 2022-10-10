ce42ff6c6945f9d0003a4bf4e2655546f046f8f4
Roman Matanin <romashka951@gmail.com>
2022-10-10 14:18:21 +0300

    Создание классов компонента представлений 681

---
+++ TemplateProject/Pages/Cities.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Linq;
+using TemplateProject.Models;
+
+namespace TemplateProject.Pages
+{
+    [ViewComponent (Name = "CitiesPageHybrid")]
+    public class CitiesModel : PageModel
+    {
+        public CitiesData Data { get; set; }
+
+        public CitiesModel(CitiesData cities)
+        {
+            Data = cities;
+        }
+
+        [ViewComponentContext]
+        public ViewComponentContext Context { get; set; }
+
+        public IViewComponentResult Invoke()
+        {
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<CityViewModel>(
+                    Context.ViewData,
+                    new CityViewModel { Cities = Data.Cities.Count(), Population = Data.Cities.Sum(c => c.Population) }
+                    )
+            };
+        }
+    }
+}
