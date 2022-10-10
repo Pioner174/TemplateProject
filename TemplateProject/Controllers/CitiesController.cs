b03de586b666851343a5f77cf057601fd5682c9f
Roman Matanin <romashka951@gmail.com>
2022-10-10 14:35:58 +0300

    Создание гибридного класса контроллера, конец главы 24,  683

---
+++ TemplateProject/Controllers/CitiesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Linq;
+using TemplateProject.Models;
+
+namespace TemplateProject.Controllers
+{
+    [ViewComponent(Name = "CitiesControllerHybrid")]
+    public class CitiesController : Controller
+    {
+        private CitiesData _data { get; set; }
+
+        public CitiesController(CitiesData data)
+        {
+            _data = data;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(_data.Cities);
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<CityViewModel>(
+                    ViewData,
+                    new CityViewModel { Cities = _data.Cities.Count(), Population = _data.Cities.Sum(c => c.Population) }
+                    )
+            };
+        }
+    }
+}
+
+
+
+
