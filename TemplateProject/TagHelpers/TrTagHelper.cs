5218d2a9604ff7a50441c6243f742bec82033341
Roman Matanin <romashka951@gmail.com>
2022-10-11 21:23:01 +0300

    Расширенные возможности вспомогательных функций дескрипторов 697

---
+++ TemplateProject/TagHelpers/TrTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TemplateProject.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+
+    [HtmlTargetElement("tr", Attributes ="bg-color,text-color")]
+    [HtmlTargetElement("td", Attributes = "bg-color")]
+    public class TrTagHelper : TagHelper
+    {
+        public string BgColor { get; set; } = "dark";
+
+        public string TextColor { get; set; } = "white";
+
+        /// <summary>
+        /// Трансформирует указанные взначение в класс 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="output"></param>
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", $"bg-{BgColor} text-center text-{TextColor}");
+        }  
+    }
+}
