bf78e7ce8351112f12d3fa383e5d83dbc42d2ec3
Roman Matanin <romashka951@gmail.com>
2022-10-12 12:00:43 +0300

    Создание элементов программным образом 700

---
+++ TemplateProject/TagHelpers/TableHeadTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace TemplateProject.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("tablehead")]
+    public class TableHeadTagHelper : TagHelper
+    {
+        public string BgColor { get; set; } = "light"; 
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "thead";
+            
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.SetAttribute("class", $"bg-{BgColor} text-white text-centr");
+
+            string content  = (await output.GetChildContentAsync()).GetContent();
+
+            output.Content.SetHtmlContent($"<tr><th colspan=\"2\">{content}</th></tr>");
+
+
+        }
+    }
+}
