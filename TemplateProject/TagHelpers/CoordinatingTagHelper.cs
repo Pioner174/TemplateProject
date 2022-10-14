1657cbbb2084810fc529cf18651fe9a4bacdf9f1
Roman Matanin <romashka951@gmail.com>
2022-10-14 13:25:46 +0300

    Подавление выходного элемента 714

---
+++ TemplateProject/TagHelpers/CoordinatingTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TemplateProject.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("tr", Attributes ="theme")]
+    public class RowTagHelper : TagHelper
+    {
+        public string Theme { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            context.Items["theme"] = Theme;
+        }
+    }
+
+    [HtmlTargetElement("th")]
+    [HtmlTargetElement("td")]
+    public class CellTagHelper : TagHelper
+    {
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context.Items.ContainsKey("theme"))
+            {
+                output.Attributes.SetAttribute("class", $"bg-{context.Items["theme"]} text-white");
+            }
+        }
+    }
+}
---
+++ TemplateProject/TagHelpers/SelectiveTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TemplateProject.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("div", Attributes ="show-when-gt, for")]
+    public class SelectiveTagHelper : TagHelper
+    {
+        public decimal ShowWhenGt { get; set; }
+
+        public ModelExpression For { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if(For.Model.GetType() == typeof(decimal) && (decimal)For.Model <= ShowWhenGt)
+            {
+                output.SuppressOutput();
+            }
+            
+        }
+    }
+}
