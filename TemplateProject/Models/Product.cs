7d40ffae035964a0af6878e907d1ef9d68776b8f
Roman Matanin <romashka951@gmail.com>
2022-10-23 15:18:07 +0300

    Форматирование элементов input

--- TemplateProject/Models/Product.cs
+++ TemplateProject/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TemplateProject.Models
 {
@@ -9,6 +10,7 @@ namespace TemplateProject.Models
         public string Name { get; set; }
 
         [Column (TypeName ="decimal(8,2)")]
+        [DisplayFormat(DataFormatString="{0:c2}", ApplyFormatInEditMode =true)]
         public decimal Price { get; set; }
 
 
