6958e2d79d6cb9b0c8c8931e4e5d8300e399a2fd
Roman Matanin <romashka951@gmail.com>
2022-10-23 16:09:33 +0300

    Работа с элементами select и option 776

--- TemplateProject/Controllers/FormController.cs
+++ TemplateProject/Controllers/FormController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +18,8 @@ namespace TemplateProject.Controllers
 
         public async Task<IActionResult> Index(long id = 1)
         {
+            ViewBag.Categories = new SelectList(_dataContext.Categories, "CategoryId", "Name");
+
             return View("Form", await _dataContext.Products.Include(p => p.Category).Include(p => p.Supplier)
                 .FirstAsync(p => p.ProductId == id));
         }
