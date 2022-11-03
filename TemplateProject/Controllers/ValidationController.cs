b19ac0f7c176e1e1fde92630b434063e7c3a0cca
Roman Matanin <romashka951@gmail.com>
2022-11-03 12:51:40 +0300

    Выполнение удаленной проверки достоверности в страницах Razor, Конец главы 29, 862

--- TemplateProject/Controllers/ValidationController.cs
+++ TemplateProject/Controllers/ValidationController.cs
@@ -15,18 +15,25 @@ namespace TemplateProject.Controllers
         }
 
         [HttpGet("categorykey")]
-        public bool CategoryKey(string categoryid)
+        public bool CategoryKey(string categoryid, [FromQuery] KeyTarget target)
         {
             long keyVal;
 
-            return long.TryParse(categoryid, out keyVal) && _dataContext.Categories.Find(keyVal) != null;
+            return long.TryParse(categoryid ?? target.CategoryId, out keyVal) && _dataContext.Categories.Find(keyVal) != null;
         }
 
         [HttpGet("supplierkey")]
-        public bool SupplierKey(string supplierId)
+        public bool SupplierKey(string supplierId, [FromQuery] KeyTarget target)
         {
             long keyVal;
-            return long.TryParse(supplierId, out keyVal) && _dataContext.Suppliers.Find(keyVal) != null;
+            return long.TryParse(supplierId ?? target.SupplierId, out keyVal) && _dataContext.Suppliers.Find(keyVal) != null;
         }
+    }
+
+    [Bind(Prefix ="Product")]
+    public class KeyTarget
+    {
+        public string CategoryId { get; set; }
+        public string SupplierId { get; set; }
     }
 }
