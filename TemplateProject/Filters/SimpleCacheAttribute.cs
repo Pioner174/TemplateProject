eaccc70d3571b77fe25d58244162ef9210863095
Roman Matanin <romashka951@gmail.com>
2022-11-09 12:37:50 +0300

    Ассинхронный фильтр ресурсов 881

--- TemplateProject/Filters/SimpleCacheAttribute.cs
+++ TemplateProject/Filters/SimpleCacheAttribute.cs
@@ -3,30 +3,29 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace TemplateProject.Filters
 {
-    public class SimpleCacheAttribute : Attribute, IResourceFilter
+    public class SimpleCacheAttribute : Attribute, IAsyncResourceFilter
     {
         private Dictionary<PathString, IActionResult> CashedResponses = new Dictionary<PathString, IActionResult>();
 
-        
-
-        public void OnResourceExecuting(ResourceExecutingContext context)
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
         {
             PathString path = context.HttpContext.Request.Path;
 
-            if (CashedResponses.ContainsKey(path))
+            if(CashedResponses.ContainsKey(path))
             {
-                context.Result = CashedResponses[path];
-
+                context.Result= CashedResponses[path];
                 CashedResponses.Remove(path);
             }
-        }
+            else
+            {
+                ResourceExecutedContext execContext = await next();
 
-        public void OnResourceExecuted(ResourceExecutedContext context)
-        {
-            CashedResponses.Add(context.HttpContext.Request.Path, context.Result);
+                CashedResponses.Add(context.HttpContext.Request.Path, execContext.Result);
+            }
         }
     }
 }
