ad1472600f2c72b6899ab93fb5bd0a1371b20819
Roman Matanin <romashka951@gmail.com>
2022-11-10 11:52:41 +0300

    Использование методов фильтров класса Controller 885

--- TemplateProject/Controllers/HomeController.cs
+++ TemplateProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using TemplateProject.Filters;
 
 namespace TemplateProject.Controllers
@@ -17,11 +18,18 @@ namespace TemplateProject.Controllers
             return View("Message", "This is the Index action on the Home controller");
         }
 
-        [ChangeArg]
+        //[ChangeArg]
         public IActionResult Messages(string message1, string message2 = "None")
         {
             return View("Message", $"{message1}, {message2}");
         }
 
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ActionArguments.ContainsKey("message1"))
+            {
+                context.ActionArguments["message1"] = "New message";
+            }
+        }
     }
 }
