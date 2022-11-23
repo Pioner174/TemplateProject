76f88bf76d6915fb862f862a8c13876226a79879
Roman Matanin <romashka951@gmail.com>
2022-11-23 17:54:50 +0300

    Создание приложения RazorPage c формами 933

---
+++ TemplateProject/Pages/EditorPageModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections;
+using System.Collections.Generic;
+using TemplateProject.Models;
+
+namespace TemplateProject.Pages
+{
+    public class EditorPageModel : PageModel
+    {
+        public DataContext DataContext { get; set; }
+
+        public ProductViewModel ViewModel { get; set; }
+
+        public IEnumerable<Category> Categories => DataContext.Categories;
+
+        public IEnumerable<Supplier> Suppliers => DataContext.Suppliers;
+
+        public EditorPageModel(DataContext dbContext)
+        {
+            DataContext = dbContext;
+        }
+    }
+}
